21ca7f6e285269cb6a9339080526dfe63af0df93
Muntasir-Mahmud <m.siyam529@gmail.com>
2024-07-07 00:38:25 +0600

    added pysqlite3-binary

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ BASE_URL = os.getenv('BASE_URL')
 PAGE_ID = os.getenv('PAGE_ID')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 
+__import__('pysqlite3')
+import sys
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 app = FastAPI(
     title="Customer Support Chatbot",
