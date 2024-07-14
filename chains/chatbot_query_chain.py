c6da7086c5447036a0b24123797b75fa084e6a0d
Muntasir-Mahmud <m.siyam529@gmail.com>
2024-07-15 00:43:11 +0600

    changed the RAG and the prompt

--- chains/chatbot_query_chain.py
+++ chains/chatbot_query_chain.py
@@ -1,46 +1,48 @@
 from langchain.chains import (create_history_aware_retriever,
                               create_retrieval_chain)
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.chat_message_histories.sql import \
     SQLChatMessageHistory
 from langchain_community.document_loaders import Docx2txtLoader
 from langchain_community.vectorstores import Chroma
-from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import MessagesPlaceholder, PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_google_genai import (ChatGoogleGenerativeAI,
                                     GoogleGenerativeAIEmbeddings,
                                     HarmBlockThreshold, HarmCategory)
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 GOOGLE_API_KEY = "AIzaSyAdVC2DwLqu0Mhufn2N4AlX-Ab6Wrk_eBw"
 
-__import__('pysqlite3')
-import sys
+# __import__('pysqlite3')
+# import sys
 
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 # TODO: add dotenv for Goolge api key
 # TODO: read the article from the python langchain neo4j
 
 def initialize_rag_chain(google_api_key=GOOGLE_API_KEY):
-    llm = ChatGoogleGenerativeAI(model="gemini-1.0-pro-latest",
+    llm = ChatGoogleGenerativeAI(model="gemini-pro",
                                 temperature=0.1,
-                                google_api_key=google_api_key,
-                                convert_system_message_to_human=True)
+                                google_api_key=google_api_key)
 
-    contextualize_system_prompt = """Given a chat history and the latest user question \
-    which might reference context in the chat history, formulate a standalone question \
-    which can be understood without the chat history. Do NOT answer the question, \
-    just reformulate it if needed and otherwise return it as is."""
+    # llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 
 
-    qa_system_prompt = """You are a customer support assistant tasked with answering questions.
-    Use the provided context to respond to the questions.
-    If the context doesn't contain the answer, simply state that you don't know. Do not attempt to create an answer.
-    Always respond in Bengali.
-    Maintain a friendly and professional tone. Answer in your own words and keep it concise.
+    qa_system_prompt = """Act as a Customer support operator of a tech shop who is very professional and concise with word and knows Bengali.\
+    This shop sells Cloud Based Attendance machine in Bangladesh. \
+    Use the following pieces of context to answer the question at the end. \
+    If you don't know the answer, just say that you don't know, don't try to make up an answer. \
+    For bigger answer give a step by step solution. \
+    Always give answer in Bengali. \
+    You tone should be more friendly and professional.\
+    You can answer in your own words. 
+    Question: {question}
     {context}
     """
 
@@ -55,36 +57,16 @@ def initialize_rag_chain(google_api_key=GOOGLE_API_KEY):
 
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=GOOGLE_API_KEY)
 
-    vector_index = Chroma.from_texts(texts, embeddings).as_retriever(search_kwargs={"k":1})
+    retriever = Chroma.from_texts(texts, embeddings).as_retriever(search_kwargs={"k":1})
+    
+    prompt = PromptTemplate.from_template(qa_system_prompt)
 
-
-
-
-    contextualize_q_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", contextualize_system_prompt),
-            MessagesPlaceholder("chat_history"),
-            ("human", "{input}"),
-        ]
+    rag_chain = (
+        {"context": retriever , "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
     )
-
-    history_aware_retriever = create_history_aware_retriever(
-        llm, vector_index, contextualize_q_prompt
-    )
-
-
-
-    qa_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", qa_system_prompt),
-            MessagesPlaceholder("chat_history"),
-            ("human", "{input}"),
-        ]
-    )
-
-    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-
-    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
     return rag_chain
 
@@ -103,24 +85,24 @@ def chatbot_executor(query: str, session_str: str):
         dict: A dictionary containing the input query and the chatbot's response.
     """
     # Initialize the chatbot with message history
-    chain_with_history = RunnableWithMessageHistory(
-        chain,
-        lambda session_id: SQLChatMessageHistory(
-            session_id=session_id, connection_string="sqlite:///sqlite.db"
-        ),
-        input_messages_key="input",
-        history_messages_key="chat_history",
-        output_messages_key="answer",
-    )
+    # chain_with_history = RunnableWithMessageHistory(
+    #     chain,
+    #     lambda session_id: SQLChatMessageHistory(
+    #         session_id=session_id, connection_string="sqlite:///sqlite.db"
+    #     ),
+    #     input_messages_key="input",
+    #     history_messages_key="chat_history",
+    #     output_messages_key="answer",
+    # )
 
-    # Set the configuration for the chatbot
-    config = {"configurable": {"session_id": session_str}}
+    # # Set the configuration for the chatbot
+    # config = {"configurable": {"session_id": session_str}}
 
     # Invoke the chatbot with the query and configuration
-    answer = chain_with_history.invoke({"input": query}, config=config)
+    answer = chain.invoke(query)
 
     # Return the input query and the chatbot's response
-    return {"input": query, "output": answer["answer"]}
+    return {"input": query, "output": answer}
 
 
 # previoue_prompt= """You are a customer support agent for Stellar Automation Limited, a company that sells smart attendance devices. You will provide chat support in Bengali.
